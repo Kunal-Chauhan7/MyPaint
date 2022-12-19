a69e0f2e9a2984baa6d1cb9f25bcf1b5e6b7bf0b
Kunal Chauhan <kunalchauhanlion@gmail.com>
2022-12-19 15:04:14 +0530

    made a mouse event handler and declare varibale

--- MyPaint/Form1.cs
+++ MyPaint/Form1.cs
@@ -12,9 +12,25 @@ namespace MyPaint
 {
     public partial class Form1 : Form
     {
+        Bitmap bmp = new Bitmap(1024, 768);
+        Pen p = new Pen(Color.Black,5);
+        bool drawing = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                drawing= false;
+            }
+            else
+            {
+                drawing = true; 
+            }
         }
     }
 }
